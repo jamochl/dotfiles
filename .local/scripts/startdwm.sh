cdcd99979476442256e966603f15e12965e550f1
JamesCHLim <james.lim.ori@gmail.com>
2020-07-25 23:50:35 +1000

    tweaked color scheme, tweaked dwmstart script

--- .local/scripts/startdwm.sh
+++ .local/scripts/startdwm.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 xrdb -merge ~/.Xresources &
-xrandr --noprimary --output eDP-1 --auto --output HDMI-1 --mode 1920x1080 --rate 75 --above eDP-1
 sxhkd -c ~/.config/dwm/sxhkdrc &
 picom &
 xwallpaper --zoom ~/.config/wallpaper &
 dunst &
-xset s 900 900
+xset s 900
 xss-lock -- slock &
-nm-applet &
+
+while true ; do
+    nm-applet
+done &
 
 while true ; do 
     ~/.local/scripts/bar_script.sh; sleep 5;
 done &
 
 while true; do
-    dwm || exit
+    dwm || pkill startdwm.sh
 done
