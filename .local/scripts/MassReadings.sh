1a0634ca7070ac8a5c22d13dcebc68f907a06af7
James C. H. Lim <james.lim.ori@gmail.com>
2020-11-08 23:56:33 +1100

    changed aliases to match capitals for directories, upgraded MassReadings script to allow input of days before and after current date.

--- .local/scripts/MassReadings.sh
+++ .local/scripts/MassReadings.sh
@@ -6,13 +6,21 @@ clean_up() {
 trap clean_up EXIT
 TEMP_FILE="/tmp/tmp.MassReadingInput"
 TEMP_FILE2="/tmp/tmp.MassReadingOutput"
-w3m -dump "https://universalis.com/australia/1100/mass.htm" > "$TEMP_FILE"
+if [ ! -z $1 ]; then
+    mass_date="$(date -d "$1 day" "+%Y%m%d")"
+else
+    mass_date="$(date "+%Y%m%d")"
+fi
+w3m -dump "https://universalis.com/australia/${mass_date}/mass.htm" > "$TEMP_FILE"
+linestart_pre="$(grep -n "Hours$" "$TEMP_FILE")"
+linestart_pre="${linestart_pre%%:*}"
+linestart_pre="$(($linestart_pre - 1))"
 linestart="$(grep -n "Readings at Mass$" "$TEMP_FILE")"
 linestart="${linestart%%:*}"
 endline="$(grep -n "The readings on this page" "$TEMP_FILE")"
 endline="${endline%%:*}"
 endline="$(( $endline - 1 ))"
-sed -n "${linestart},${endline}p" "$TEMP_FILE" > "$TEMP_FILE2"
+sed -n "1,${linestart_pre}p;${linestart},${endline}p" "$TEMP_FILE" > "$TEMP_FILE2"
 sed -i \
     '6,$s/━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━/\n&/g' \
     "$TEMP_FILE2"
