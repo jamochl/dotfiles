2bb6c85f288fb3c2b57313ce186c7ce151c04454
James C. H. Lim <james.lim.ori@gmail.com>
2020-11-12 08:03:40 +1100

    changed mass_readings script to use functional concepts of immutable variables, removing the need for temporary files, cleanup traps etc.

--- .local/scripts/mass_readings.sh
+++ .local/scripts/mass_readings.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 set -e
-clean_up() {
-    rm $TEMP_FILE $TEMP_FILE2
-}
-trap clean_up EXIT
-TEMP_FILE="/tmp/tmp.MassReadingInput"
-TEMP_FILE2="/tmp/tmp.MassReadingOutput"
-if [ ! -z $1 ]; then
+if [ ! -z "$1" ]; then
     mass_date="$(date -d "$1 day" "+%Y%m%d")"
 else
     mass_date="$(date "+%Y%m%d")"
 fi
-w3m -dump "https://universalis.com/australia/${mass_date}/mass.htm" > "$TEMP_FILE"
-linestart_pre="$(grep -n "Hours$" "$TEMP_FILE")"
-linestart_pre="${linestart_pre%%:*}"
-linestart_pre="$(($linestart_pre - 1))"
-linestart="$(grep -n "Readings at Mass$" "$TEMP_FILE")"
-linestart="${linestart%%:*}"
-endline="$(grep -n "The readings on this page" "$TEMP_FILE")"
-endline="${endline%%:*}"
-endline="$(( $endline - 1 ))"
-sed -n "1,${linestart_pre}p;${linestart},${endline}p" "$TEMP_FILE" > "$TEMP_FILE2"
-sed -i \
+html_data="$(w3m -dump "https://universalis.com/australia/${mass_date}/mass.htm")"
+linestart_pre="$(echo "$html_data" | grep -n "Hours$")"
+linestart_pre2="${linestart_pre%%:*}"
+linestart_pre3="$(("$linestart_pre2" - 1))"
+linestart="$(echo "$html_data" | grep -n "Readings at Mass$")"
+linestart2="${linestart%%:*}"
+endline="$(echo "$html_data" | grep -n "^The readings on this page")"
+endline2="${endline%%:*}"
+endline3="$(("$endline2" - 1))"
+echo "$html_data" | sed -n "1,${linestart_pre3}p;${linestart2},${endline3}p" | sed \
     '6,$s/━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━/\n&/g' \
-    "$TEMP_FILE2"
-LESSSECURE=1 less "$TEMP_FILE2"
+| LESSSECURE=1 less
 exit 0
