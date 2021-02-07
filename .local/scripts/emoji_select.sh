de7a9539537d7890d28f52d2ecb5675f1ca380df
James C. H. Lim <james.lim.ori@gmail.com>
2021-02-07 21:26:22 +1100

    moved emoji.txt out of my scripts into extras folder, fixed emoji_select script to match new position.

--- .local/scripts/emoji_select.sh
+++ .local/scripts/emoji_select.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rofi -font 'FiraCode Nerd Font 12' -dmenu < $HOME/.local/scripts/emoji.txt | awk '{ print $1 }' | tr -d '\n' | xclip -sel clip
+rofi -font 'FiraCode Nerd Font 12' -dmenu < $HOME/.local/extras/emoji.txt | awk '{ print $1 }' | tr -d '\n' | xclip -sel clip
