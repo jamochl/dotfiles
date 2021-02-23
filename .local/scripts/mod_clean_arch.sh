628c18d3dddeec884f3de9c1b89a829ccf2370fe
James C. H. Lim <james.lim.ori@gmail.com>
2021-02-24 10:27:06 +1100

    comment rearrange for mod_clean_arch.sh

--- .local/scripts/mod_clean_arch.sh
+++ .local/scripts/mod_clean_arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# A script to clean up old kernel modules (not current kernel and not newly
-# installed) in the /usr/lib/modules directory. Necessary as I want to be able
-# to continue using the PC after a kernel update
+# A script to clean up old kernel modules (not current kernel and not
+# newly installed) in the /usr/lib/modules directory. Necessary as I
+# want to be able to continue using the PC after a kernel update
 
 # Install in /etc/pacman.d/scripts
 # Call from a alpm path post-hook at /etc/pacman.d/hooks
@@ -9,12 +9,12 @@
 set -e
 
 CURRENT_KERN="$(uname -r)"
-# LINUX and LINUX_LTS actually do not give exactly the correct values, the
-# version is eg. 5.10.3.arch1-1, but the module directory name is
-# 5.10.3-arch1-1. In a regular expression with grep this is fine, because a dot
-# globs every character, but it will not work with the `ls` --hide option
-# unless you process the version to change the '.' to a '-', which is not worth
-# it
+# LINUX and LINUX_LTS actually do not give exactly the correct values,
+# the pacman version is eg. 5.10.3.arch1-1, but the module directory
+# name is 5.10.3-arch1-1. In a regular expression with grep this is
+# fine, because a dot globs every character, but it will not work with
+# the `ls` --hide option unless you process the version to change the
+# '.' to a '-', which is not worth it
 LINUX="$(pacman -Qi linux | awk '/Version/ { print $3 }')"
 LINUX_LTS="$(pacman -Qi linux-lts | awk '/Version/ { print $3 "-lts" }')"
 OLD_MODULES="$(ls /usr/lib/modules | grep -e "$LINUX" -e "$LINUX_LTS" -e "$CURRENT_KERN" --invert-match)"
