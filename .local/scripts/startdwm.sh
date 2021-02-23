95e6a8624b975e4b9e762d983de70fb15b1f6895
James C. H. Lim <james.lim.ori@gmail.com>
2021-02-24 10:20:20 +1100

    changed picom in startup script and keyboard binding to use glx backends and vsync

--- .local/scripts/startdwm.sh
+++ .local/scripts/startdwm.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 autorandr -c
-[ -f ~/.Xresources  ] && xrdb -merge ~/.Xresources &
-[ -f ~/.Xmodmap ] && xmodmap ~/.Xmodmap &
-[ -f ~/.config/dwm/sxhkdrc ] && sxhkd -c ~/.config/dwm/sxhkdrc &
-[ -f ~/.config/wallpaper ] && xwallpaper --zoom ~/.config/wallpaper &
-picom &
+[[ -f ~/.Xresources  ]] && xrdb -merge ~/.Xresources &
+[[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap &
+[[ -f ~/.config/dwm/sxhkdrc ]] && sxhkd -c ~/.config/dwm/sxhkdrc &
+[[ -f ~/.config/wallpaper ]] && xwallpaper --zoom ~/.config/wallpaper &
+
+picom --backend glx --vsync &
 dunst &
 xset s 900 &
 xset s noblank &
