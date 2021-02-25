3e79625faeee7a60f14de07bd0c1d7d2a8044f2c
James C. H. Lim <james.lim.ori@gmail.com>
2021-02-26 10:28:24 +1100

    further specified backup config

--- .local/scripts/backup.sh
+++ .local/scripts/backup.sh
@@ -5,4 +5,4 @@ R_DELETE="--delete"
 BACKUP_DIR="backup@192.168.1.215:metabox_14/"
 SOURCE_DIR="/home/james/"
 
-rsync "$R_OPTS" "$R_DELETE" --exclude={"/.steam","/.cache","/.trash"} "$SOURCE_DIR" -e ssh "$BACKUP_DIR"
+rsync "$R_OPTS" "$R_DELETE" --include={"/.local/","/.config/","/.ssh/"} --exclude="/.*/" "$SOURCE_DIR" -e ssh "$BACKUP_DIR"
