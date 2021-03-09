a680e46d755f8d8a0437dda5706d6e9589ae010b
James C. H. Lim <james.lim.ori@gmail.com>
2021-03-09 14:44:02 +1100

    created a script to easily edit any git tracked config file using dmenu

---
+++ .local/scripts/edit_config.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dotfiles() {
+    /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@
+}
+cd
+selected_script="$(dotfiles ls-files | \
+     dmenu)"
+
+echo $selected_script
+
+if [[ ! -z $selected_script ]]; then
+    alacritty -e vim "$HOME/$selected_script"
+fi
