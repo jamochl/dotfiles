40135895dabbc04fe2efea96dad41d0c970ea442
jamochl <james.lim@jamochl.com>
2022-01-23 11:59:33 +1100

    Make mass_readings script center text based on terminal width

--- .local/user_scripts/mass_readings.sh
+++ .local/user_scripts/mass_readings.sh
@@ -7,13 +7,17 @@ else
 fi
 mass_url="https://universalis.com/australia/${mass_date}/mass.htm"
 
+default_width=80
+num_spaces="$((($(tput cols) - $default_width) / 2))"
+insert_string=$(python3 -c "print (' ' * $num_spaces)")
+
 cache_file="/tmp/${mass_date}_mass"
 if [ -f "$cache_file" ]; then
-    LESSSECURE=1 less "$cache_file"
+    sed "s/^/$insert_string/g" "$cache_file" | LESSSECURE=1 less
     exit
 fi
 
-html_data="$mass_url\n$(w3m -dump "$mass_url")"
+html_data="$mass_url\n$(w3m -cols $default_width -dump "$mass_url")"
 linestart_pre="$(echo "$html_data" | awk "/Hours$/ { print NR }")"
 linestart_pre2="$(("$linestart_pre" - 1))"
 linestart="$(echo "$html_data" | awk "/Readings at Mass$/ { print NR }")"
@@ -22,5 +26,6 @@ endline2="$(("$endline" - 1))"
 echo -e "$html_data" | sed -n "1,${linestart_pre2}p;${linestart},${endline2}p" | \
   sed "$linestart,\$s/^━/\\n&/g" | \
   tee "$cache_file" | \
+  sed "s/^/$insert_string/g" | \
   LESSSECURE=1 less
 exit 0
